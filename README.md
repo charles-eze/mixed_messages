d7dca3d4e29bc0376f6f652e7760afe54365c0eb
charles-eze <charleseze.github.com>
2021-01-17 19:18:23 +0100

    This is my first commit for this project

---
+++ README.md
@@ -0,0 +1,30 @@
+# Mixed Messages
+> This program generates a random output everytime a user runs it.
+
+## Table of contents
+* [General Info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Status](#status)
+* [Inspiration](#inspiration)
+
+## General info
+This project consists of an object initialized with an array of messages. A random function is then created which will use the length of the arrays to select random messages that will be displayed to the console. 
+
+
+## Technologies
+* Javascript - ES6
+* Git and GitHub
+* Command Line
+
+## Setup
+To run this program, you must have node.js installed.
+> To run:  `$ node script.js`
+
+
+## Status
+Project is: _completed_
+
+## Inspiration
+This is a Codecademy portfolio project. Learning on Codecademy has inspired me to continually develop myself and strive to become a better developer.
+
---
+++ script.js
@@ -0,0 +1,30 @@
+// Creating a messages object with an array properties.
+const messages = {
+    intro: [
+      'Hello, welcome to this amazing repo!',
+      'This is the moment you have been waiting for.',
+      'What would life be without developers?',
+      'Oh no! how can you be so awesome?',
+      'Would you love to see some magic?'
+    ],
+    exit: [
+      'Look around you, there is so much to learn.',
+      'Congratulations, it is your time to shine!',
+      'I mean, there would be no javascript, oh no!!!',
+      'It seems you were created on a sunday? you look gorgeous.',
+      'Oh well, get me a computer and put some js on it.'
+    ]
+  }
+  
+// get a random index number;
+let randomIndex = num => {
+    return Math.floor(Math.random() * num)
+}
+
+
+ // get random messages
+const random1 = `${messages.intro[randomIndex(messages.intro.length)]}`
+const random2 = ` ${messages.exit[randomIndex(messages.exit.length)]} `
+  
+  // log the messages to console
+  console.log(random1 , 'Oh hold on a sec!',random2)
